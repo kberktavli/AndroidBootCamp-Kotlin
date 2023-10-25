f60d802d17685a33b71b3ac1fa3c94174f879b5b
kberktavli <kberktavli@gmail.com>
2023-10-25 17:53:59 +0300

    five commit - trycatch structure

--- app/src/main/java/com/berktavli/kotlinlessons/ileri/HataYakalama.kt
+++ app/src/main/java/com/berktavli/kotlinlessons/ileri/HataYakalama.kt
@@ -1,2 +1,22 @@
 package com.berktavli.kotlinlessons.ileri
 
+import java.lang.Exception
+
+fun main() {
+    //1.Compile Error
+    val x = 10
+    //x = 300
+
+    //2.Exception : Runtime Error
+    val a = 10
+    val b = 0
+
+
+    try {
+        println("Sonuç : ${a / b}")
+        println("İşlem Tamam")
+    }catch (e : Exception){
+        println("İkinci sayı sıfır olamaz.")
+    }
+
+}
